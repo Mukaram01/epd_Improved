19c07b50d99844f06c0d350477c728f9bf48f1b0
notanexpert <121873648+Mukaram01@users.noreply.github.com>
2026-09-06 12:58:13 +0100

    EPD-5: enable Profiles and Replay workflow

--- easy_perception_deployment/easy_perception_deployment/gui/main.py
+++ easy_perception_deployment/easy_perception_deployment/gui/main.py
@@ -25,6 +25,7 @@ from windows.epd1_productization import apply_epd1_productization
 from windows.epd2_productization import apply_epd2_productization
 from windows.epd3_productization import apply_epd3_productization
 from windows.epd4_productization import apply_epd4_productization
+from windows.epd5_productization import apply_epd5_productization
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 myapp = QApplication(sys.argv)
@@ -38,6 +39,7 @@ def main():
     apply_epd2_productization(window1)
     apply_epd3_productization(window1)
     apply_epd4_productization(window1)
+    apply_epd5_productization(window1)
     window1.help_window.setWindowFlag(Qt.Window, True)
     window1.show()
 
